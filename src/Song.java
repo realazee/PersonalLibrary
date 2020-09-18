55fa025b81edfa295f084f6c3dbb6842728191d4
Anton Cheng <100023295@mvla.net>
2020-09-18 10:51:13 -0700

    Wrote Book and Song classes

--- src/Song.java
+++ src/Song.java
@@ -1,8 +1,41 @@
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Song.
+ */
 public class Song extends Media {
-
-	public Song() {
-		// TODO Auto-generated constructor stub
+	
+	/** The artist. */
+	private String artist;
+	private String genre;
+	
+	/**
+	 * Instantiates a new song.
+	 */
+	public Song(String title) {
+		super(title);
 	}
+	public String toString() {
+		String str = "artist: " + this.getArtist() + "\n"
+					+"Song Title: " + this.getTitle() + "\n"
+					+"Genre: " + this.getGenre() + "\n"
+					+"Format: " + this.getFormat() + "\n"
+					+"Location: " + this.getLocation() + "\n"
+					+"Notes: " + this.getNotes();
+		return str;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
 
 }
