d65dc958e60d6aa782c81f882b61af238bcd1d1f
Ethan Peng <100023497@mvla.net>
2020-09-18 10:46:29 -0700

    Updated Media for getters and setters and constructors.

--- src/Media.java
+++ src/Media.java
@@ -1,9 +1,63 @@
 import java.util.Comparator;
 
 public class Media implements Comparable<Media>, Comparator<Media> {
+		
+	private String title;
+	private String location;
+	private String notes;
+	private String format;
+	
+	
+	public String getTitle() {
+		return title;
+	}
 
-	public Media() {
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+	public String getNotes() {
+		return notes;
+	}
+
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+
+	public String getFormat() {
+		return format;
+	}
+
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+
+	public Media(String title, String location, String notes, String format) {
 		// TODO Auto-generated constructor stub
+		this.title = title;
+		this.location = location;
+		this.notes = notes;
+		this.format = format;
+	}
+	public Media(String title) {
+		// TODO Auto-generated constructor stub
+		this.title = title;
 	}
 
 	
