ed858c87bf8dae60f3ba521f5bff64b86b93f2d4
Aaron Zheng <100024608@mvla.net>
2020-09-29 10:04:13 -0700

    Completed PersonalLibraryController for use cases 1 and 2

--- src/PersonalLibraryController.java
+++ src/PersonalLibraryController.java
@@ -1,18 +1,59 @@
 
 public class PersonalLibraryController {
-
-	
+	PersonalLibraryModel model;
+	String err;
 	public PersonalLibraryController() {
-		PersonalLibraryModel model = new PersonalLibraryModel();
+		model = new PersonalLibraryModel();
+
 	}
-	
-	public void addMedia() {
-		
+
+	public String addBook(String author, String title, String format, String location, String notes) {
+		err = checkForBlankTitle(title);
+		if(err.length() == 0) {
+			model.insertBook(author, title, format, location, notes);
+		}
+		return err;
+
 	}
-	
+	public String addSong(String author, String songTitle, String format, String location, String notes) {
+		err = checkForBlankTitle(songTitle);
+		if(err.length() == 0) {
+			model.insertSong(author, songTitle, format, location, notes);
+		}
+		return err;
+	}
+	public String addVideo(String vidTitle, String star, String format, String location, String notes) {
+		err = checkForBlankTitle(vidTitle);
+		if(err.length() == 0) {
+			model.insertVideo(vidTitle, star, format, location, notes);
+		}
+		return err;
+	}
+	public String addVideoGame(String gtitle, String format, String location, String notes) {
+		err = checkForBlankTitle(gtitle);
+		if(err.length() == 0) {
+		model.insertVideoGame(gtitle, format, location, notes);
+		}
+		return err;
+
+	}
+
+
 	//add error checking
-	//add the add functions
-	
-	
+
+
+
+
+	public String checkForBlankTitle(String title) {
+		if(title.length() == 0) {
+			return "The title cannot be blank! Media will not be entered into personal library.";
+
+		}
+
+		return "";
+
+	}
+
+
 
 }
