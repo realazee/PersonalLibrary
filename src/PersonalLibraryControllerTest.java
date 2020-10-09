f3495b731462805b7b8246d40656bf4b68ca4f43
Aaron Zheng <100024608@mvla.net>
2020-10-08 20:04:26 -0700

    Wrote most of Junit testing, some tests incomplete

--- src/PersonalLibraryControllerTest.java
+++ src/PersonalLibraryControllerTest.java
@@ -45,8 +45,61 @@ class PersonalLibraryControllerTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_AddVideoWithBlankTitle() {
+		PersonalLibraryController ctrl = new PersonalLibraryController();
+		ctrl.addVideo("","","","","");
+		assertTrue(ctrl.model.getMedia().length == 0);
+		assertTrue(ctrl.addVideo("","","","","").equals("The title cannot be blank! Media will not be entered into personal library."));
+		
+	}
+	@Test
+	void test_AddVideoGameWithBlankTitle() {
+		PersonalLibraryController ctrl = new PersonalLibraryController();
+		ctrl.addVideoGame("","","","");
+		assertTrue(ctrl.model.getMedia().length == 0);
+		assertTrue(ctrl.addVideoGame("","","","").equals("The title cannot be blank! Media will not be entered into personal library."));
+		
+	}
+	
+	void test_AddSongWithBlankTitle() {
+		PersonalLibraryController ctrl = new PersonalLibraryController();
+		ctrl.addSong("","","","","","");
+		assertTrue(ctrl.model.getMedia().length == 0);
+		assertTrue(ctrl.addSong("","","","","","").equals("The title cannot be blank! Media will not be entered into personal library."));
+		
+	}
+	@Test
+	void test_AddBookWithBlankTitle() {
+		PersonalLibraryController ctrl = new PersonalLibraryController();
+		ctrl.addBook("","","","","");
+		assertTrue(ctrl.model.getMedia().length == 0);
+		assertTrue(ctrl.addBook("","","","","").equals("The title cannot be blank! Media will not be entered into personal library."));
+		
+	}
+	@Test
+	void test_SearchbyType() {
+		PersonalLibraryController ctrl = new PersonalLibraryController();
+		ctrl.addBook("testbook","","","","");
+		ctrl.addSong("testsong","","","","","");
+		ctrl.addBook("testvideo","","","","");
+		ctrl.addVideoGame("testvideogame","","","");
+		assertTrue(ctrl.getMediaByType("Book").length == 1);
+		assertTrue(ctrl.getMediaByType("Song").length == 1);
+		assertTrue(ctrl.getMediaByType("Video Game").length == 1);
+		assertTrue(ctrl.getMediaByType("Book").length == 1);
+			
+	}
+	@Test
+	void test_SearchbyTitle() {
+		PersonalLibraryController ctrl = new PersonalLibraryController();
+		ctrl.addBook("testbook","","","","");
+		ctrl.addSong("testsong","","","","","");
+		ctrl.addBook("testvideo","","","","");
+		ctrl.addVideoGame("testvideogame","","","");
+		assertTrue(ctrl.getMediaByTitle("testbook").length == 1);
+		assertTrue(ctrl.getMediaByTitle("testsong").length == 1);
+		assertTrue(ctrl.getMediaByTitle("testvideo").length == 1);
+		assertTrue(ctrl.getMediaByTitle("testvideogame").length == 1);
 	}
 
 }
